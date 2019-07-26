94a738d81a158a1339e84a678e765aee1f46ca31
Yasmani Palmero <yasmany.07@gmail.com>
2019-07-26 11:59:37 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+    const buttonText = { text: 'click me' };
+    const labelText = 'Enter name:';
+
     return (
         <div>
-            <label for="name" class="label">Enter name:</label>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor:'blue', color: 'white' }}>Submit</button>
+            <button style={{ backgroundColor:'blue', color: 'white' }}>
+                {buttonText.text}
+            </button>
         </div>
     );
 };
